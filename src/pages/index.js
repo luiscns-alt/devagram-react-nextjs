4c20bb1412b9c80e72dd2c19a7e3fdfce204b09d
Luis Carlos <luiscarlosnors@gmail.com>
2022-10-23 21:09:58 -0300

    feat: validadoes e componente de botão e avatar.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,15 @@
+import Avatar from '../componentes/avatar'
+import Botao from '../componentes/botao'
+
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <Botao
+        texto={'Login'}
+        manipularClique={() => console.log('Click no botão')}
+      />
+      <Avatar />
     </div>
   )
 }
