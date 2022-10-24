4bd56ec117b7607aaa6abf53a2dc4de8e508e033
Luis Carlos <luiscarlosnors@gmail.com>
2022-10-23 21:35:59 -0300

    feat: Componente Upload imagem.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,7 @@
-import Avatar from '../componentes/avatar'
-import Botao from '../componentes/botao'
-
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <Botao
-        texto={'Login'}
-        manipularClique={() => console.log('Click no botão')}
-      />
-      <Avatar />
     </div>
   )
 }
